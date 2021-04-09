ba6886878cbc6b7d44b0eb3fc271efa9b496d79c
prashikjaware <prashikjaware123@gmail.com>
2021-04-09 17:45:46 +0530

    [Prashik]Added: Add multiple values to the attributes of tables

---
+++ AddressBook/AddValues.sql
@@ -0,0 +1,9 @@
+use Address_Book
+
+insert into address_book_system values
+(
+'Prashik','Jaware','Home','Buldhana','Maharashtra','443444','987654321','prashikjaware@gmail.com'
+),
+(
+'Konbde','Sahed','Home','Pune','Maharashtra','443403','987650235','kombdesaheb@gmail.com'
+)
