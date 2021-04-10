93c2f52093411594cb6048f7bce073e46f051abd
prashikjaware <prashikjaware123@gmail.com>
2021-04-10 15:56:25 +0530

    [Prashik]Added: Ability to understand the size of address book by city and state

---
+++ AddressBook/CountByCityOrState.sql
@@ -0,0 +1,9 @@
+use Address_Book
+
+select * from address_book_system
+
+select count(phone_number) 'COUNT' from address_book_system
+
+select count(state) 'COUNT' from address_book_system
+
+select count(city) 'COUNT' from address_book_system
