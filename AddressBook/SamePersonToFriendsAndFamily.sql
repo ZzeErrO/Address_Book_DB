ad33bdf2f777c8a0ab9f0ea5c83b04af0b8dff1d
prashikjaware <prashikjaware123@gmail.com>
2021-04-17 18:13:00 +0530

    [Prashik]Added: Ability to add person to both Friend and Family

---
+++ AddressBook/SamePersonToFriendsAndFamily.sql
@@ -0,0 +1,8 @@
+use Address_Book
+
+select * from address_book_system
+
+insert into address_book_system values
+(
+'Mango','Strawberry','Love','Heart','People','100001','111111111','mangostrawberrylove@gmail.com', 'address2', 'friends'
+)
